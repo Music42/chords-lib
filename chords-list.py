386c3260f5033008384d937fcd3861da16bd23bf
@gpupo <g@g1mr.com>
2016-12-08 16:03:45 -0200

    title, composer

--- chords-list.py
+++ chords-list.py
@@ -9,35 +9,54 @@ def buildChord(forte, a):
         c.transpose(a, True)
 
     if c.isMajorTriad():
-        c.addLyric(c.pitchedCommonName+'M')
+        mode = "M"
     elif c.isMinorTriad():
-        c.addLyric(c.pitchedCommonName+'m')
+        mode = "m"
     else:
-        c.addLyric(c.pitchedCommonName+'?')
+        mode = ""
 
+    legend = c.pitchedCommonName.replace('--', 'b ').replace('-', ' ')+' '+mode
+    #c.fullName
+    c.addLyric(legend)
     c.duration.type = 'whole'
 
     return c;
 
+def buildInterval(fromNote, toNote):
+    dc = note.Note(fromNote)
+    pc = note.Note(toNote)
+    return interval.Interval(dc, pc)
 
-for forte in ['3-5', '3-11', '7-35', '8-28']:
-    for i in range(12):
-        c = buildChord(forte, i)
-        d[c.pitchedCommonName] = c
+for mainNote in ['c', 'c#','d', 'd#', 'e-', 'e', 'f', 'f#', 'g', 'g#', 'a-', 'a', 'a#', 'b-', 'b', 'c-']:
+    sc = scale.ChromaticScale(mainNote+'3')
+    for direction in ['ascending', 'descending']:
+        for p in sc.getPitches(mainNote+'3', mainNote+'4', direction=direction):
+            i = buildInterval(mainNote+'3', p)
+            #'8-28'
+            for forte in ['3-5', '3-11', '7-35']:
+                c = buildChord(forte, i)
+                d[c.fullName] = c
 
 dict = collections.OrderedDict(sorted(d.items(), key=lambda t: t[0]))
 
-display = stream.Stream()
+s = stream.Stream()
+p = stream.Part()
+m = stream.Measure()
+
+s.insert(0, metadata.Metadata())
+s.metadata.title = 'Chords Lib'
+s.metadata.composer = 'music42'
+
+p.insert(0, metadata.Metadata())
+p.metadata.title = 'List'
 
 ts0 = meter.TimeSignature('4/4')
-display.append(ts0)
-
+p.append(ts0)
 
 for key, value in dict.items():
+    p.append(value);
     for x in value.derivation.chain():
-        display.append(x);
+        p.append(x);
 
-    display.append(value);
-
-
-display.show()
+s.append(p)
+s.show()
